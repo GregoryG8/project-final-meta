cf925d7d7a1d8e0ed8cb540b6b2a832dad8d4d24
GregoryG8 <79380668+GregoryG8@users.noreply.github.com>
2024-07-29 18:29:42 -0500

    feat: create Home's view

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Home from './views/Home';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+      <Home/>
+    </>
   );
 }
 
