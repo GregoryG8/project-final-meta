6b5df539747c5bca0225be151caf7f067d630f24
GregoryG8 <79380668+GregoryG8@users.noreply.github.com>
2024-07-29 18:27:35 -0500

    feat: create navigation bar

---
+++ src/components/Navbar/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./NavBar.css"
+import logo from "../../assets/Logo.svg"
+
+function NavBar() {
+  return (
+    <header>
+        <nav>
+            <img src={logo} alt="Logo" />
+            <ul className='page-links-container karla'>
+              <li><a href="#home">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#menu">Menu</a></li>
+              <li><a href="#reservations">Reservations</a></li>
+              <li><a href="#order">Order Online</a></li>
+              <li><a href="#login">Login</a></li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default NavBar
