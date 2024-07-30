1374083dfe891cc97972a9588c89618aa1216af2
GregoryG8 <79380668+GregoryG8@users.noreply.github.com>
2024-07-30 17:42:58 -0500

    feat: add Hero component in Home view

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "../components/Navbar/NavBar";
+import Hero from "../components/Hero/Hero";
 
 function Home() {
   return (
     <>
       <NavBar />
+      <Hero/>
     </>
   );
 }
