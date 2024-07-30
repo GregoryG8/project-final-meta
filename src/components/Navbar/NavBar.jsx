af17a98d6f443ff5951a5a99501ae9cfdfd48100
GregoryG8 <79380668+GregoryG8@users.noreply.github.com>
2024-07-30 17:37:05 -0500

    refactor: add classes in navbar files

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -6,7 +6,7 @@ function NavBar() {
   return (
     <header>
         <nav>
-            <img src={logo} alt="Logo" />
+            <img className='logo' src={logo} alt="Logo" />
             <ul className='page-links-container karla'>
               <li><a href="#home">Home</a></li>
               <li><a href="#about">About</a></li>
