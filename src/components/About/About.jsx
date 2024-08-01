f171dd5ddef5fce55c67b4c9f27046be17f03b3d
GregoryG8 <79380668+GregoryG8@users.noreply.github.com>
2024-07-31 23:39:45 -0500

    feat: create About component

---
+++ src/components/About/About.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+  return (
+    <section>
+      <div className="restaurant-info">
+        <h1>Little Lemon</h1>
+        <p className="subtitle-about">Chicago</p>
+        <p className="description-about">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+        <div className="images-about">
+            <img src="" alt="" />
+            <img src="" alt="" />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About;
