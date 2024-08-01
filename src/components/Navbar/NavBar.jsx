cc36246ad1d3f5f6c7104bcf953a79644695ef27
GregoryG8 <79380668+GregoryG8@users.noreply.github.com>
2024-07-31 22:27:20 -0500

    refactor: add responsive design

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,23 +1,46 @@
-import React from 'react'
-import "./NavBar.css"
-import logo from "../../assets/Logo.svg"
+import React, { useState } from "react";
+import "./NavBar.css";
+import logo from "../../assets/Logo.svg";
+import hamburger from "../../assets/hamburger.svg";
 
 function NavBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen); // Cambia el estado a su opuesto
+  };
   return (
     <header>
-        <nav>
-            <img className='logo' src={logo} alt="Logo" />
-            <ul className='page-links-container karla'>
-              <li><a href="#home">Home</a></li>
-              <li><a href="#about">About</a></li>
-              <li><a href="#menu">Menu</a></li>
-              <li><a href="#reservations">Reservations</a></li>
-              <li><a href="#order">Order Online</a></li>
-              <li><a href="#login">Login</a></li>
-            </ul>
-        </nav>
+      <nav>
+        <img className="logo" src={logo} alt="Logo" />
+        <ul
+          className={`page-links-container karla ${
+            isMenuOpen ? "show" : "hidden"
+          }`}
+        >
+          <li className="links">
+            <a href="#home">Home</a> <span></span>
+          </li>
+          <li className="links">
+            <a href="#about">About</a> <span></span>
+          </li>
+          <li className="links">
+            <a href="#menu">Menu</a> <span></span>
+          </li>
+          <li className="links">
+            <a href="#reservations">Reservations</a> <span></span>
+          </li>
+          <li className="links">
+            <a href="#order">Order Online</a> <span></span>
+          </li>
+          <li className="links">
+            <a href="#login">Login</a> <span></span>
+          </li>
+        </ul>
+          <img className="hamburger" src={hamburger} alt="" onClick={toggleMenu}/>
+
+      </nav>
     </header>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
