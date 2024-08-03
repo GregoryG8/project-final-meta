5fdf5eb6e5e299aa203e1f52cf705d6967ef315a
GregoryG8 <79380668+GregoryG8@users.noreply.github.com>
2024-08-03 18:46:59 -0500

    style: add style and images for About component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import "./About.css";
+import aboutImage1 from "../../assets/Mario and Adrian A.jpg"
+import aboutImage2 from "../../assets/Mario and Adrian b.jpg"
 
 function About() {
   return (
-    <section>
+    <section className="about">
       <div className="restaurant-info">
-        <h1>Little Lemon</h1>
-        <p className="subtitle-about">Chicago</p>
-        <p className="description-about">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
+        <h1 className="title-about markazi">Little Lemon</h1>
+        <p className="subtitle-about markazi">Chicago</p>
+        <p className="description-about karla">
+          Welcome to our family-owned Mediterranean restaurant. We blend
+          traditional recipes with a modern twist, offering a unique dining
+          experience that celebrates both heritage and innovation. Join us and
+          savor the rich flavors of our Mediterranean cuisine, crafted with love
+          and a contemporary flair.
         </p>
-        <div className="images-about">
-            <img src="" alt="" />
-            <img src="" alt="" />
-        </div>
+      </div>
+      <div className="images-about">
+        <img src={aboutImage1} alt="" />
+        <img src={aboutImage2} alt="" />
       </div>
     </section>
   );
