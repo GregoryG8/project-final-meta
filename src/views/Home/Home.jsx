8ce26cbdbed83cc7446a0ede639e8f4e293367af
GregoryG8 <79380668+GregoryG8@users.noreply.github.com>
2024-08-04 18:27:15 -0500

    style: configure structure

---
+++ src/views/Home/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import NavBar from "../../components/Navbar/NavBar";
+import Hero from "../../components/Hero/Hero";
+import Highlights from "../../components/Highlights/Highlights";
+import Testimonials from "../../components/Testimonials/Testimonials";
+import About from "../../components/About/About";
+import Footer from "../../components/Footer/Footer";
+
+function Home() {
+  return (
+    <>
+      <NavBar/>
+      <Hero/>
+      <Highlights/>
+      <Testimonials/>
+      <About/>
+      <Footer/>
+    </>
+  );
+}
+
+export default Home;
