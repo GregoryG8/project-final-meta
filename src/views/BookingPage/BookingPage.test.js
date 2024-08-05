ad8944be6e85cee6c348ff9d0e937d8fe27150e0
GregoryG8 <79380668+GregoryG8@users.noreply.github.com>
2024-08-04 23:00:04 -0500

    test: create test for BookingPage

---
+++ src/views/BookingPage/BookingPage.test.js
@@ -0,0 +1,34 @@
+// BookingPage.test.js
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import BookingPage from "./BookingPage";
+import { fetchAPI } from "../../components/api/api";
+import { MemoryRouter } from "react-router-dom";
+
+// Mock de fetchAPI
+jest.mock("../../components/api/api", () => ({
+  fetchAPI: jest.fn(),
+}));
+
+test("initializeTimes updates available times correctly", async () => {
+  // Datos mockeados para fetchAPI
+  const mockTimes = ["17:00", "18:00", "19:00"];
+  fetchAPI.mockReturnValue(mockTimes);
+
+  render(
+    <MemoryRouter>
+      <BookingPage />
+    </MemoryRouter>
+  );
+
+  // Espera a que al menos uno de los tiempos disponibles esté en el DOM
+  await waitFor(() => {
+    expect(screen.getByText("17:00")).toBeInTheDocument();
+  });
+
+  // Verifica la presencia de otros tiempos disponibles
+  expect(screen.getByText("18:00")).toBeInTheDocument();
+  expect(screen.getByText("19:00")).toBeInTheDocument();
+});
+
+
